b3358c98cb1eadc302ee4990839b89d4055296b9
aliganji14 <aliganji142879@gmail.com>
2026-06-21 15:00:29 +0330

    feat: implement weather fetching endpoint with caching support

--- core/main.py
+++ core/main.py
@@ -6,6 +6,12 @@ from users.routes import router as users_routes
 from i18n_routes import router as i18n_routes
 from core.database import Base, engine
 from core.exceptions import ExpenseNotFoundException
+from core.config import settings
+import httpx
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
 
 tags_metadata = [
     {
@@ -51,8 +57,47 @@ app.include_router(i18n_routes)
 
 
 @app.exception_handler(ExpenseNotFoundException)
-async def expense_not_found_exception_handler(request: Request, exc: ExpenseNotFoundException):
+async def expense_not_found_exception_handler(
+    request: Request, exc: ExpenseNotFoundException
+):
     return JSONResponse(
         status_code=404,
         content={"status": "error", "message": exc.message},
     )
+
+
+redis = aioredis.from_url(settings.REDIS_URL)
+cache_backend = RedisBackend(redis)
+FastAPICache.init(cache_backend, prefix="fastapi-cache")
+
+
+async def request_current_weather(latitude: float, longitude: float):
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": "temperature_2m,relative_humidity_2m",
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        current_weather = data.get("current", {})
+        return current_weather
+    else:
+        return None
+
+
+@app.get("/fetch-current-weather", status_code=200)
+@cache(expire=10)
+async def fetch_current_weather(latitude: float = 40.7128, longitude: float = -74.0060):
+    current_weather = await request_current_weather(latitude, longitude)
+    if current_weather:
+
+        return JSONResponse(content={"current_weather": current_weather})
+    else:
+        return JSONResponse(
+            content={"detail": "Failed to fetch weather"}, status_code=500
+        )
